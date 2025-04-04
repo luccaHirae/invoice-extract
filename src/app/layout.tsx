1a6b95dfd184fa9a4d37685a4d29851bdf2906e2
Bruno Hirae <brunodeluca117@outlook.com>
2025-04-04 20:28:33 -0300

    feat: integrate @tanstack/react-query for data fetching and add Providers component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next';
 import { Geist, Geist_Mono } from 'next/font/google';
 import { Sidebar } from '@/components/sidebar';
+import Providers from '@/app/providers';
 import './globals.css';
 
 const geistSans = Geist({
@@ -28,7 +29,9 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <Sidebar>{children}</Sidebar>
+        <Sidebar>
+          <Providers>{children}</Providers>
+        </Sidebar>
       </body>
     </html>
   );
