3a42f17282ec08728fa5a491142bd8285bec32bc
Bruno Hirae <brunodeluca117@outlook.com>
2025-04-04 21:53:13 -0300

    feat: add formatCurrency utility function for currency formatting

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,3 +4,10 @@ import { twMerge } from 'tailwind-merge';
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
+
+export function formatCurrency(value: number, locale = 'pt-BR') {
+  return new Intl.NumberFormat(locale, {
+    style: 'currency',
+    currency: 'BRL',
+  }).format(value);
+}
