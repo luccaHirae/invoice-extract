d26e31fe0c8f37d1b6047120f01273ae140b2bea
Bruno Hirae <brunodeluca117@outlook.com>
2025-04-06 11:47:03 -0300

    feat: enhance loading state in InvoicesPage with spinner

--- src/app/biblioteca/page.tsx
+++ src/app/biblioteca/page.tsx
@@ -1,9 +1,16 @@
 import { ClientsTable } from '@/components/clients-table';
+import { Loader2 } from 'lucide-react';
 import { Suspense } from 'react';
 
 export default function InvoicesPage() {
   return (
-    <Suspense fallback={<div>Loading...</div>}>
+    <Suspense
+      fallback={
+        <div className='flex h-screen w-full items-center justify-center'>
+          <Loader2 className='animate-spin' />
+        </div>
+      }
+    >
       <ClientsTable />
     </Suspense>
   );
